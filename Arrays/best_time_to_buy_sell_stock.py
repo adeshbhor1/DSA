031221e6146fac19a9c9adbaad90f72385e52290
Adesh Bhor <adeshbhor1@gmail.com>
2026-03-24 17:55:19 +0530

    Fix logic and optimize stock profit solution

--- Arrays/best_time_to_buy_sell_stock.py
+++ Arrays/best_time_to_buy_sell_stock.py
@@ -1,14 +1,14 @@
-# #Brute Force Approach O(n^2)
-# def maxProfit(prices):
-#     max_profit = 0
+#Brute Force Approach O(n^2)
+def maxProfit(prices):
+    max_profit = 0
     
-#     for i in range(0, len(prices)):
-#         for j in range(i + 1, len(prices)):
-#             if prices[j] > prices[i]:
-#                 profit = prices[j] - prices[i]
-#                 max_profit = max(max_profit, profit)
+    for i in range(0, len(prices)):
+        for j in range(i + 1, len(prices)):
+            if prices[j] > prices[i]:
+                profit = prices[j] - prices[i]
+                max_profit = max(max_profit, profit)
                 
-#     return max_profit
+    return max_profit
 
 #Optimized Approach O(n)
 def maxProfit(prices):
