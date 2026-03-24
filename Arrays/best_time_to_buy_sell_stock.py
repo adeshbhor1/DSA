08e6678283c15b2bea83c23ed212b1afc0347f1c
Adesh Bhor <adeshbhor1@gmail.com>
2026-03-24 17:48:05 +0530

    Added optimal solution for stock problem

---
+++ Arrays/best_time_to_buy_sell_stock.py
@@ -0,0 +1,26 @@
+# #Brute Force Approach O(n^2)
+# def maxProfit(prices):
+#     max_profit = 0
+    
+#     for i in range(0, len(prices)):
+#         for j in range(i + 1, len(prices)):
+#             if prices[j] > prices[i]:
+#                 profit = prices[j] - prices[i]
+#                 max_profit = max(max_profit, profit)
+                
+#     return max_profit
+
+#Optimized Approach O(n)
+def maxProfit(prices):
+    min_price = float('inf')
+    max_profit = 0
+    
+    for price in prices:
+        min_price = min(min_price, price)
+        profit = price - min_price
+        max_profit = max(max_profit, profit)
+    return max_profit
+
+prices = [7,2,1,5,6,8]
+print(maxProfit(prices))
+                
