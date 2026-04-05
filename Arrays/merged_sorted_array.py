eb9315523fa944f3bc3c05807c2b47100187e705
Adesh Bhor <adeshbhor1@gmail.com>
2026-04-05 22:35:31 +0530

    Add optimal approach for merged sorted array

--- Arrays/merged_sorted_array.py
+++ Arrays/merged_sorted_array.py
@@ -22,3 +22,33 @@ nums2 = [2,5,6]
 n = 3
 
 print(merge(nums1, m, nums2, n))
+
+# Optimized Approach Tc =O(m+n) , Sc = O(1)
+def mergeSortedArray(num1, m, num2, n):
+    
+    i = m - 1
+    j = n - 1
+    k = m + n - 1
+    
+    while i >= 0 and j >= 0:
+        if num1[i] > num2[j]:
+            num1[k] = num1[i]
+            i -= 1
+        else:
+            num1[k] = num2[j]
+            j -= 1
+        k -= 1
+        
+    while j >= 0:
+        num1[k] = num2[j]
+        j -= 1
+        k -= 1
+    return num1
+
+num1 = [1,2,3,0,0,0]
+m = 3
+
+num2 = [2,5,6]
+n = 3
+
+print(mergeSortedArray(num1, m, num2, n))
