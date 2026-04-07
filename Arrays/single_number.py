68bc022cc87f6e4cae4298455697a81c2b41c788
Adesh Bhor <adeshbhor1@gmail.com>
2026-04-07 11:05:21 +0530

    Add Single Number solutions using brute force and XOR approach

---
+++ Arrays/single_number.py
@@ -0,0 +1,26 @@
+#Brute Force Approach Tc = O(n^2), Sc = O(n) 
+def singleNumber(nums):
+    
+    for i in range(len(nums)):
+        count = 0
+        
+        for j in range(len(nums)):
+            if nums[i] == nums[j]:
+                count += 1
+        
+        if count == 1:
+            return nums[i]
+
+nums = [2,2,1,3,1]
+print(singleNumber(nums))
+
+#Optimized Approach Tc = O(n), Sc =O(1)
+def singleNumbers(nums):
+    
+    result = 0
+    for num in nums:
+        result ^= num
+    return result
+
+nums = [1,1,3,3,4]
+print(singleNumbers(nums))
