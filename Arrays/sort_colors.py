c94a74e8be1e4bfa508d096714fd857561d331e8
Adesh Bhor <adeshbhor1@gmail.com>
2026-04-14 22:23:10 +0530

    Implemented sortColors using brute force and optimized two-pass approach

--- Arrays/sort_colors.py
+++ Arrays/sort_colors.py
@@ -1,13 +1,32 @@
 #Brute Force Aproach Tc = O(n^2), Sc = O(1)
-def sortColors(nums):
+def sortColors(num):
     
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[i] > nums[j]:
-                nums[i], nums[j] = nums[j], nums[i]
-    return nums
+    for i in range(len(num)):
+        for j in range(i + 1, len(num)):
+            if num[i] > num[j]:
+                num[i], num[j] = num[j], num[i]
+    return num
         
-nums = [4,2,5,2,0,0,1]
+num = [4,2,5,2,0,0,1]
 
-print(sortColors(nums))
+print(sortColors(num))
 
+# Optimized Approach Tc = O(n), Sc = O(1)
+def sortColor(nums):
+    n = len(nums)
+    index = 0
+
+    for i in range(n):
+        if nums[i] == 0:
+            nums[i], nums[index] = nums[index], nums[i]
+            index += 1
+
+    for i in range(index, n):
+        if nums[i] == 1:
+            nums[i], nums[index] = nums[index], nums[i]
+            index += 1
+    
+    return nums
+
+nums = [2,0,2,1,1,0]
+print(sortColor(nums))
