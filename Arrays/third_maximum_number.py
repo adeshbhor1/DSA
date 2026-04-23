838cfa333fc4b441b54f67c2e025e219eb5fe71c
Adesh Bhor <adeshbhor1@gmail.com>
2026-04-23 22:18:26 +0530

    Add the optimized approach for third maximum number

--- Arrays/third_maximum_number.py
+++ Arrays/third_maximum_number.py
@@ -20,3 +20,25 @@ def thirdMaximumNumber(num):
 print(thirdMaximumNumber([3,2,1]))      
 print(thirdMaximumNumber([1,2]))        
 print(thirdMaximumNumber([2,2,3,1])) 
+
+# Optimized Approach Tc = O(n), Sc = O(1)
+
+def thirdMax(nums):
+
+    top3 = set()
+
+    for num in nums:
+        top3.add(num)
+
+        if len(top3) > 3:
+            top3.remove(min(top3))
+
+    if len(top3) == 3:
+        return min(top3)
+
+    return max(top3)
+
+
+print(thirdMax([3,2,1]))      
+print(thirdMax([1,2]))        
+print(thirdMax([2,2,3,1]))    
